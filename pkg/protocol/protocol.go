99adb540c13b55f83dd8043ff1c95fbf488fc175
Miles Frankel <miles.frankel@gmail.com>
2023-07-30 16:39:49 -0400

    wip

--- pkg/protocol/protocol.go
+++ pkg/protocol/protocol.go
@@ -28,7 +28,7 @@ func (h *Header) MarshalBinary() ([]byte, error) {
 }
 
 // TODO: api?
-func ParseHeader(rdr io.Reader) (*Header, error) {
+func ParseHeader(rdr protocolReader) (*Header, error) {
 	h := &Header{}
 	err := binary.Read(rdr, binary.BigEndian, h)
 	if err != nil {
@@ -61,7 +61,7 @@ func (q *Question) MarshalBinary() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func ParseQuestion(rdr io.Reader) (*Question, error) {
+func ParseQuestion(rdr protocolReader) (*Question, error) {
 	var err error
 	q := &Question{}
 	if q.Name, err = decodeName(rdr); err != nil {
@@ -86,33 +86,76 @@ func encodeName(name string) []byte {
 	return append(encoded, 0)
 }
 
-// TODO: api? also is this right
-func decodeName(rdr io.Reader) (string, error) {
-	ln := make([]byte, 1)
-	part := make([]byte, 64)
-	parts := make([]string, 0, 2)
-	for {
-		// read length byte
-		_, err := rdr.Read(ln)
-		if err != nil {
-			return "", fmt.Errorf("reading length: %w", err)
-		}
-		length := ln[0]
-		if length == 0 {
-			break
-		}
-		// read that many bytes
-		n, err := rdr.Read(part[:length])
-		if err != nil {
-			return "", fmt.Errorf("reading part: %w", err)
-		}
-		if n != int(length) {
-			return "", fmt.Errorf("reading part: expected %d bytes, got %d", length, n)
-		}
+type protocolReader interface {
+	io.Reader
+	io.ByteReader
+	io.Seeker
+}
 
-		parts = append(parts, string(part[:length]))
+// TODO: api? also is this right
+gfunc decodeName(rdr protocolReader) (string, error) {
+	var (
+		length byte
+		err    error
+		part   = make([]byte, 64)
+		parts  = make([]string, 0, 2)
+	)
+	for length, err = rdr.ReadByte(); err == nil && length > 0; length, err = rdr.ReadByte() {
+		if length&0b1100_0000 > 0 { // compressed
+			if err := decodeCompressedNameInto(length, rdr, parts); err != nil {
+				return "", fmt.Errorf("decoding compressed name: %w", err)
+			}
+		} else {
+			// read that many bytes
+			n, err := rdr.Read(part[:length])
+			if err != nil {
+				return "", fmt.Errorf("reading part: %w", err)
+			}
+			if n != int(length) {
+				return "", fmt.Errorf("reading part: expected %d bytes, got %d", length, n)
+			}
+
+			parts = append(parts, string(part[:length]))
+		}
+	}
+	if err != nil {
+		return "", fmt.Errorf("reading length: %w", err)
 	}
 	return strings.Join(parts, "."), nil
+}
+
+func decodeCompressedNameInto(length byte, rdr protocolReader, parts []string) error {
+	next, err := rdr.ReadByte()
+	if err != nil {
+		return fmt.Errorf("reading next byte for ptr: %w", err)
+	}
+
+	ptrBs := []byte{length & 0b0011_1111, next}
+	var ptr uint16
+	if err := binary.Read(bytes.NewReader(ptrBs), binary.BigEndian, &ptr); err != nil {
+		return fmt.Errorf("casting ptr: %w", err)
+	}
+
+	// seek to pos "ptr", decode a name, seek back
+	// note that a compressed name should never point to another compressed name, so this shouldnt recurse forever
+	// but we may want to be more defensive, TODO
+	current, err := rdr.Seek(0, io.SeekCurrent)
+	if err != nil {
+		return fmt.Errorf("getting pos: %w", err)
+	}
+	if _, err := rdr.Seek(int64(ptr), io.SeekStart); err != nil {
+		return fmt.Errorf("seeking to ptr: %w", err)
+	}
+	name, err := decodeName(rdr)
+	if err != nil {
+		return fmt.Errorf("decoding name: %w", err)
+	}
+	if _, err := rdr.Seek(current, io.SeekStart); err != nil {
+		return fmt.Errorf("seeking back: %w", err)
+	}
+	parts = append(parts, name)
+
+	return nil
 }
 
 const (
@@ -204,7 +247,7 @@ func (r *Record) MarshalBinary() ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func ParseRecord(rdr io.Reader) (*Record, error) {
+func ParseRecord(rdr protocolReader) (*Record, error) {
 	var err error
 	r := &Record{}
 	if r.Name, err = decodeName(rdr); err != nil {
